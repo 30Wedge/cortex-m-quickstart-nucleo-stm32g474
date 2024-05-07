94f5f1955a8bb603f27187e9ad9390674d475959
30Wedge <amagregor.2018@comcast.net>
2024-05-07 14:06:11 -0400

    panic example

--- examples/panic.rs
+++ examples/panic.rs
@@ -9,18 +9,34 @@
 
 // Pick one of these panic handlers:
 
-// `panic!` halts execution; the panic message is ignored
+/* `panic!` halts execution by dropping into an infinite loop.
+ * The panic message is ignored. If a debugger is connected, then you still
+ * get to see a backtrace of where the panic happened.
+ */
 use panic_halt as _;
 
-// Reports panic messages to the host stderr using semihosting
-// NOTE to use this you need to uncomment the `panic-semihosting` dependency in Cargo.toml
-// use panic_semihosting as _;
+/* Reports panic messages to the host stderr using semihosting
+ * It gives you a nicely formatted error message like this:
+ * """
+ * panicked at examples/panic.rs:33:5:
+ * Oops
+ * """
+ * and the debugger stops at the 'panic' call with a neat backtrace
+ *
+ * NOTE to use this you need to import `panic-semihosting` in Cargo.toml
+ */
+//use panic_semihosting as _;
 
-// Logs panic messages using the ITM (Instrumentation Trace Macrocell)
-// NOTE to use this you need to uncomment the `panic-itm` dependency in Cargo.toml
+/* Logs panic messages using the ITM (Instrumentation Trace Macrocell)
+ * NOTE to use this you need to uncomment the `panic-itm` dependency in Cargo.toml
+ */
 // use panic_itm as _;
 
 use cortex_m_rt::entry;
+
+/* Include stm32g474 library to link in exception handlers */
+#[allow(unused_imports)]
+use stm32g4::stm32g474;
 
 #[entry]
 fn main() -> ! {
