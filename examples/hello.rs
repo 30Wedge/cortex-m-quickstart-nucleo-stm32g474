cf79946bf6e440f4ebf88333fba4fd7154829aef
30Wedge <amagregor.2018@comcast.net>
2024-05-07 14:00:39 -0400

    hello world example

--- examples/hello.rs
+++ examples/hello.rs
@@ -6,15 +6,14 @@
 use panic_halt as _;
 
 use cortex_m_rt::entry;
-use cortex_m_semihosting::{debug, hprintln};
+use cortex_m_semihosting::hprintln;
+
+/* Include stm32g071 library to link in exception handlers */
+#[allow(unused_imports)]
+use stm32g0::stm32g071;
 
 #[entry]
 fn main() -> ! {
     hprintln!("Hello, world!").unwrap();
-
-    // exit QEMU
-    // NOTE do not run this on hardware; it can corrupt OpenOCD state
-    //debug::exit(debug::EXIT_SUCCESS);
-
     loop {}
 }
